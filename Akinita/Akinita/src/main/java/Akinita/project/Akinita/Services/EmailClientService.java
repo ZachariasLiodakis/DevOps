43229f203c89df62550c0d32dd4a7b18f6396be7
Zacharias Liodakis <liodakiszaharias@gmail.com>
2025-06-25 20:49:17 +0300

    error with email-Akinita services when run using docker due to different ip addresses

--- Akinita/Akinita/src/main/java/Akinita/project/Akinita/Services/EmailClientService.java
+++ Akinita/Akinita/src/main/java/Akinita/project/Akinita/Services/EmailClientService.java
@@ -1,6 +1,7 @@
 package Akinita.project.Akinita.Services;
 
 import Akinita.project.Akinita.EmailRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -8,14 +9,16 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class EmailClientService {
 
-    private final String EMAIL_SERVICE_URL = "http://localhost:8081/api/email/send";
+    @Value("${email.service.url}")
+    private String emailServiceUrl;
 
     private final RestTemplate restTemplate = new RestTemplate();
 
     public boolean sendEmail(String to, String subject, String content) {
         EmailRequest request = new EmailRequest(to, subject, content);
         try {
-            ResponseEntity<String> response = restTemplate.postForEntity(EMAIL_SERVICE_URL, request, String.class);
+            ResponseEntity<String> response = restTemplate.postForEntity(
+                    emailServiceUrl + "/api/email/send", request, String.class);
             return response.getStatusCode().is2xxSuccessful();
         } catch (Exception e) {
             System.err.println("Failed to call email-service: " + e.getMessage());
